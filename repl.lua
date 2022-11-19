c46a9b97f024bf2aea518b146c8cc4d1f63843b4
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-11-19 11:03:44 +0100

    rework stuff, wip prompt cursor

--- repl.lua
+++ repl.lua
@@ -13,23 +13,85 @@ local redraw_clock = nil
 
 
 -- ------------------------------------------------------------------------
--- STATE
+-- STATE - UI
 
 local page_list = {'MAIDEN', 'SC'}
 local pages = UI.Pages.new(1, #page_list)
 local current_repl = 'MAIDEN'
 
+
+-- ------------------------------------------------------------------------
+-- STATE - PROMPTS
+
+-- TODO: store then in a table to prevent all those if/else shenanigans
+
+-- local prompts = {
+--   MAIDEN={
+--     text = "",
+--     cursor = 0,
+--   },
+--   SC ={
+--     text = "",
+--     cursor = 0,
+--   },
+-- }
+
+local maiden_repl_prompt_cursor = 0
 local maiden_repl_prompt = ""
+local sc_repl_prompt_cursor = 0
 local sc_repl_prompt = ""
 
+local function current_prompt()
+  if current_repl == 'MAIDEN' then
+    return maiden_repl_prompt
+  elseif current_repl == 'SC' then
+    return sc_repl_prompt
+  end
+end
+
+local function current_prompt_cursor()
+  if current_repl == 'MAIDEN' then
+    return maiden_repl_prompt_cursor
+  elseif current_repl == 'SC' then
+    return sc_repl_prompt_cursor
+  end
+end
+
+local function add_char_to_current_prompt(char)
+  if current_repl == 'MAIDEN' then
+    maiden_repl_prompt = maiden_repl_prompt .. char
+    maiden_repl_prompt_cursor = maiden_repl_prompt_cursor + 1
+  elseif current_repl == 'SC' then
+    sc_repl_prompt = sc_repl_prompt .. char
+    sc_repl_prompt_cursor = sc_repl_prompt_cursor + 1
+  end
+end
+
+local function remove_char_left_current_prompt()
+  if current_repl == 'MAIDEN' then
+    maiden_repl_prompt = string.sub(maiden_repl_prompt, 1, -2)
+    maiden_repl_prompt_cursor = math.max(0, maiden_repl_prompt_cursor - 1)
+  elseif current_repl == 'SC' then
+    sc_repl_prompt = string.sub(sc_repl_prompt, 1, -2)
+    sc_repl_prompt_cursor = math.max(0, sc_repl_prompt_cursor - 1)
+  end
+end
 
 -- ------------------------------------------------------------------------
 -- REPL OUTPUTS
 
 local log_buffer_length = 50
 
-local maiden_output = fifo():setempty(function() return nil end)
-local sc_output = fifo():setempty(function() return nil end)
+  local maiden_output = fifo():setempty(function() return nil end)
+  local sc_output = fifo():setempty(function() return nil end)
+
+  local function current_repl_out_buff()
+  if current_repl == 'MAIDEN' then
+    return maiden_output
+  elseif current_repl == 'SC' then
+    return sc_output
+  end
+end
 
 local function register_new_repl_output(output_buff, msg)
   for line in msg:gmatch("[^\n]+") do
@@ -71,32 +133,18 @@ end
 -- ------------------------------------------------------------------------
 -- IO - KBD - REPL PROMPTS
 
--- TODO: cursor position
-
 function keyboard.char(char)
   if char == "l" and keyboard.ctrl() then
-    if current_repl == 'MAIDEN' then
-      clear_repl_output(maiden_output)
-    elseif current_repl == 'SC' then
-      clear_repl_output(sc_output)
-    end
+    clear_repl_output(current_repl_out_buff())
     return
   end
 
-  if current_repl == 'MAIDEN' then
-    maiden_repl_prompt = maiden_repl_prompt .. char
-  elseif current_repl == 'SC' then
-    sc_repl_prompt = sc_repl_prompt .. char
-  end
+  add_char_to_current_prompt(char)
 end
 
 function keyboard.code(code, value)
   if code == 'BACKSPACE' and value > 0 then
-    if current_repl == 'MAIDEN' then
-      maiden_repl_prompt = string.sub(maiden_repl_prompt, 1, -2)
-    elseif current_repl == 'SC' then
-      sc_repl_prompt = string.sub(sc_repl_prompt, 1, -2)
-    end
+    remove_char_left_current_prompt()
   elseif code == 'ENTER' and value > 0 then
     if current_repl == 'MAIDEN' then
       repl_osc.send_maiden(maiden_repl_prompt)
@@ -116,6 +164,7 @@ end
 local max_nb_lines_to_show=5
 
 local function draw_repl_logs(repl_output)
+  screen.level(10)
   local buff_len = repl_output:length()
   local nb_lines = math.min(buff_len, max_nb_lines_to_show)
 
@@ -128,30 +177,25 @@ local function draw_repl_logs(repl_output)
   end
 end
 
+local function draw_repl_prompt(text, cursor)
+  screen.level(8)
+  screen.move(0, 60)
+  screen.text(">> ")
+  screen.level(15)
+  screen.move(15, 60)
+  screen.text(text)
+end
+
 function redraw()
   screen.clear()
 
   pages:redraw()
 
   -- logs
-  screen.level(10)
-  if current_repl == 'MAIDEN' then
-    draw_repl_logs(maiden_output)
-  elseif current_repl == 'SC' then
-    draw_repl_logs(sc_output)
-  end
+  draw_repl_logs(current_repl_out_buff())
 
   -- prompt
-  screen.level(8)
-  screen.move(0, 60)
-  screen.text(">> ")
-  screen.level(15)
-  screen.move(15, 60)
-  if current_repl == 'MAIDEN' then
-    screen.text(maiden_repl_prompt)
-  elseif current_repl == 'SC' then
-    screen.text(sc_repl_prompt)
-  end
+  draw_repl_prompt(current_prompt(), current_prompt_cursor())
 
   screen.update()
 end
